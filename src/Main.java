678bb92c60c0a3193629790929fb25b64a5e9a37
Melnikov Dmitriy <243orlr@mail.ru>
2023-08-02 14:53:01 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class Main {
+    private static String[] texts = new String[100_000];
+    private static AtomicInteger countThree = new AtomicInteger(0);
+    private static AtomicInteger countFour = new AtomicInteger(0);
+    private static AtomicInteger countFive = new AtomicInteger(0);
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    public static void generationShirtStr() {
+        Random random = new Random();
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        generationShirtStr();
+
+        new Thread(() -> {
+            synchronized (texts) {
+                for (int i = 0; i < texts.length; i++) {
+                    if (texts[i].length() == 3 & texts[i].charAt(0) == texts[i].charAt(2) & texts[i].charAt(1) == texts[i].charAt(2)) {
+                        //System.out.println(texts[i]);
+                        countThree.getAndIncrement();
+                    }
+                }
+            }
+            System.out.println("Красивых слов с длиной 3 " + countThree.get() + " шт.");
+        }).start();
+
+        new Thread(() -> {
+            synchronized (texts) {
+                for (int i = 0; i < texts.length; i++) {
+                    if (texts[i].length() == 4) {
+                        if (texts[i].charAt(0) == texts[i].charAt(3) & texts[i].charAt(1) == texts[i].charAt(2)) {
+                            //System.out.println(texts[i]);
+                            countFour.getAndIncrement();
+                        }
+                    }
+                }
+            }
+            System.out.println("Красивых слов с длиной 4 " + countFour.get() + " шт.");
+        }).start();
+
+        new Thread(() -> {
+            synchronized (texts) {
+                for (int i = 0; i < texts.length; i++) {
+                    if (texts[i].length() == 5) {
+                        if ((texts[i].charAt(0) == texts[i].charAt(1) & texts[i].charAt(3) == texts[i].charAt(4)) &
+                                (texts[i].charAt(0) == texts[i].charAt(2) || texts[i].charAt(2) == texts[i].charAt(4))) {
+                            //System.out.println(texts[i]);
+                            countFive.getAndIncrement();
+                        }
+                    }
+                }
+            }
+            System.out.println("Красивых слов с длиной 5 " + countFive.get() + " шт.");
+        }).start();
+    }
+}
